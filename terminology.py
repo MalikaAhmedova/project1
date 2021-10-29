2ac2aae98cdd81e21bfb8521db3407842fd95c0a
MalikaAhmedova <malika.a.ahmedova@gmail.com>
2021-10-28 21:21:17 -0400

    Adding new files to the project

---
+++ terminology.py
@@ -0,0 +1,14 @@
+"""
+IDE - IDE, or Integrated Development Environment, is a software application
+that combines all of the features and tools needed by a software developer.
+
+cloning - receiving the code for the fist time
+
+working copies are called branches
+
+whenever you want to share you commit (save) and push (share). If somebody wants
+to get an updated copy, they pull and update
+
+tooling
+
+"""
